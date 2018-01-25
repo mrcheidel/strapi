51b356d9f98b90316c983db78245ffaa30da1886
cohl <claudio.heidel@gft.com>
2018-01-25 17:19:54 +0100

    fix

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # strapi
 StrAPI + MongoDB on Docker
+
+# https://strapi.io/
+
+## Install MongoDb on docker
+
+	chmod 777 install_mongo.sh
+	./install_mongo.sh
--- install_mongo.sh
+++ install_mongo.sh
@@ -2,6 +2,6 @@ docker stop mongo_instance
 docker rm mongo_instance 
 docker rmi mongo_image 
 docker build -t mongo_image .
-docker run -d --name mongo_instance -h mongo_instance -p 27017:27017 -p 1337:1337 -t mongo_image
+docker run -d --name mongo_instance -h mongo_instance -p 27017:27017 -t mongo_image
 # docker exec -i -t mongo_instance /bin/bash
 
